3aaf4aab502aa3a9d293f222cfd16f02dbbeacfd
ＺＥＮＯＩ <86784191+Zenoixnoize@users.noreply.github.com>
2022-08-22 17:57:52 +0530

    Add files via upload

--- Garfield.js
+++ Garfield.js
@@ -1078,6 +1078,27 @@ case 'sex':{
              await GarfieldNeural.sendButtonText(m.chat, buttons, jawab,`Neural Ai` , m, {mentions: ments})
             }
             break
+
+            case 'getsong': {
+            
+                if (!text) return reply(`Example : ${prefix + command} kuweni`)
+                let yts = require("yt-search")
+                let search = await yts(text)
+                let anu = search.videos[Math.floor(Math.random() * 1)]       
+                var Linkx = anu.url
+               
+                let { tharinduaudio } = require('./lib/ytmp3')
+                if (!text) return reply(`Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 320kbps`)
+                let quality = args[1] ? args[1] : '320kbps'
+                let media = await tharinduaudio(Linkx, quality)
+                if (media.filesize >= 999999) return reply('File Over Limit '+util.format(media))
+                GarfieldNeural.sendImage(m.chat, media.thumb,  '```Title :``` *' + media.title + '*\n```URL :``` *' + isUrl(Linkx) + '*\n```Quality :``` *320Kbps*\n', m)
+                GarfieldNeural.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'audio/mpeg', fileName: `{media.title}.m4a` }, { quoted: m }) 
+            
+            }
+          
+            break
+      
       
             case 'hellou': {
                 let { tharinduaudio } = require('./lib/ytmp3')
